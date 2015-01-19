4da5a4b469c0aafbdddc426ac9922ecb8868f154
Ryan Hsu <ryan.hsu@ayasdi.com>
2015-01-19 15:38:16 -0800

    Completed functions

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,43 @@
 
 ## Write a short comment describing this function
 
+# Creates a special matrix that caches its inverse
+#
+# Args:
+#	x: Input matrix
 makeCacheMatrix <- function(x = matrix()) {
+	inv_ <<- NULL
+		
+	set <- function(y)	{
+		x <<- y
+		inv_ <<- NULL
+	}
+	get <- function() x
+	
+	setinv <- function(inv) inv_ <<- inv
+	getinv <- function() inv_
 
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
+# Solves and caches the input makeCacheMatrix type
+#
+# Args:
+#	x: Input matrix returned by makeCacheMatrix
+#
+# Returns:
+#	Inverse of matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+        	return (m)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setinv(inv)
+        inv
 }
